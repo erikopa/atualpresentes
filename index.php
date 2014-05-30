8b130bfe2040b210468618f1de974a8b0845a145
ErikoVerissimo <erikopa@gmail.com>
2014-05-30 07:16:06 -0300

    Commit alteração de serviço de email

--- index.php
+++ index.php
@@ -198,11 +198,46 @@ else                /* send the submitted data */
 			echo "Todos os campos são de preenchimento obrigatório, por favor preencha <a href=\'\'> o contato </a> novamente."; 
 			} 
 		else{         
-			$from="From: $name<$email>\r\nReturn-path: $email"; 
-			$subject="Mensagem enviada usando o formulário de contato: "; 
-			mail("erikopa@gmail.com", $subject, $message, $from); 
+			$subject="Mensagem enviada usando o formulário de contato"; 
+			$url = 'https://api.sendgrid.com/';
+			$user = 'erikopa';
+			$pass = '126817ev';
+			
+			$params = array(
+				'api_user'  => $user,
+				'api_key'   => $pass,
+				'to'        => 'carinenunes24@hotmail.com',
+				'subject'   => $subject,
+				'html'      => $message,
+				'from'      => $email,
+			  );
+			
+			
+			$request =  $url.'api/mail.send.json';
+			
+			// Generate curl request
+			$session = curl_init($request);
+			// Tell curl to use HTTP POST
+			curl_setopt ($session, CURLOPT_POST, true);
+			// Tell curl that this is the body of the POST
+			curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+			// Tell curl not to return headers, but do return the response
+			curl_setopt($session, CURLOPT_HEADER, false);
+			curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+			
+			// obtain response
+			$response = curl_exec($session);
+			curl_close($session);
+			
 			echo "E-mail enviado!"; 
 			} 
+			
+			
+			
+			
+			
+			
+			
     }   
 ?> 
                 </div>
